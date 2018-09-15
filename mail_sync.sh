3439e3baf8e441e0437a1b29fc32a26b9abd43e0
Subramanian <gokusubr@amazon.com>
2018-09-15 17:49:31 +0100

    Refactored to support personal gmail account

--- mail_sync.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-output=$(mbsync -qa)
-if [[ $? -ne 0 ]]; then
-  echo "Mail sync failed" | terminal-notifier -sound=default
-fi
-notmuch new
-
---
+++ mbsync/mail_sync.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+
+mbsync -c $HOME/mail-config/common-mail-config/mbsync/.mbsyncrc -a
+
